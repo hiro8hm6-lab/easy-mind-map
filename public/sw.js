e226bbab0b8c260e5534781bf0a6e94f86184c85
hiro8hm6-lab <you@example.com>
2026-09-19 23:33:28 +0900

    Fix passive wheel zoom warning

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "synapsequest-shell-v2";
+const CACHE = "synapsequest-shell-v3";
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(["/", "/favicon.svg"])).then(() => self.skipWaiting()));
 });
